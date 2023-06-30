8df944976babb84188f8d5d2a3617cdd2ab06aaa
Subrat  Tiwari <tsubrat301@gmail.com>
2023-06-30 17:08:48 +0530

    update

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/v1/product", productRoutes);
 
 app.use(express.static(path.join(__dirname,'./client/build')))
  app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname, ".client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
  });
 
 //rest api
